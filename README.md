176f6889f4fe60cfdbc19f02e81ee18c76434cad
makgab <makgab@gmail.com>
2023-04-07 20:42:17 +0200

    Initial update

---
+++ README.md
@@ -0,0 +1,32 @@
+# Symfony Blog DB app
+
+Sample apps and docs:
+
+* https://symfony.com/doc/current/doctrine.html
+
+* https://symfony.com
+* https://symfony.com/doc/
+* https://symfony.com/doc/current/setup.html
+* https://symfony.com/doc/current/forms.html
+
+
+
+Commands:
+
+ * Existing project: composer install
+ * New project: symfony new symfony_database --full
+ * php bin/console about
+ * composer require symfony/orm-pack
+ * composer require symfony/form
+ * composer require --dev symfony/maker-bundle
+ * php bin/console doctrine:database:create
+ * php bin/console make:entity
+ * php bin/console make:migration
+ * php bin/console doctrine:migrations:migrate
+ * php bin/console make:controller ProductController
+ * php bin/console dbal:run-sql 'SELECT * FROM product'
+ * php bin/console debug:router
+
+ * Run server: symfony server:start
+   http://127.0.0.1:8000
+
---
+++ src/Entity/Blog.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BlogRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BlogRepository::class)]
+class Blog
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $post = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPost(): ?string
+    {
+        return $this->post;
+    }
+
+    public function setPost(string $post): self
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
